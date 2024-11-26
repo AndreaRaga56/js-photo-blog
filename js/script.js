c847994ca5984850a626f71af1bb00c0fb996bc3
AndreaRaga56 <andrearaga56@gmail.com>
2024-11-26 16:04:18 +0100

    feat over element funzionante

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 const mainElem = document.querySelector("main");
-const overElem = document.getElementById("over-list");
+const overElem = document.querySelector(".over-list");
 let textHTMLMain = '';
-let textHTMLOver ='';
+let textHTMLOver = '';
 
-axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((resp) => {
-
-
-    for (let i = 0; i < 6; i++) {
-        let titolo = resp.data[i].title;
+function printPhoto(a) {
+    for (let i = 0; i < a.length; i++) {
+        let immagine = a[i].url;
+        let id = a[i].id;
+        let titolo = a[i].title;
         titolo = titolo[0].toUpperCase() + titolo.slice(1);
-        console.log(titolo);
-        let immagine = resp.data[i].url;
+
         textHTMLMain += `
-        <div class="ms-card card">
+        <div class="ms-card card" data-post-id='${id}'>
             <div class="pin"> <img src="./img/pin.svg" alt=""></div>
             <section> <img src="${immagine}" alt=""></section>
             <p class="desc">${titolo}</p>
         </div>
         `;
-        textHTMLOver+=`
-        <div class="over">
+
+        textHTMLOver += `
+        <div class="over d-none" data-post-id='${id}'>
             <button type="button" class="btn btn-lg btn-light"> CHIUDI </button>
             <img src="${immagine}" alt="">
         </div>
@@ -27,10 +27,33 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((resp) =>
     }
     mainElem.innerHTML = textHTMLMain;
     overElem.innerHTML = textHTMLOver;
+};
 
+function openAndCloseOver() {
+    const cardsElem = document.querySelectorAll('.card ');
+    const cardsOverElem = document.querySelectorAll('.over');
+    const btnsElem = document.querySelectorAll("button");
 
+    cardsElem.forEach(curCardElem => {
+        curCardElem.addEventListener("click", function() {
+            cardsOverElem.forEach(curOverElem => {
+                if(curCardElem.dataset.postId===curOverElem.dataset.postId){
+                    console.log(curOverElem);
+                    curOverElem.classList.remove("d-none");
+                }
+                
+                btnsElem.forEach(curBtnElem =>{
+                    curBtnElem.addEventListener("click", function(){
+                        curOverElem.classList.add("d-none");
+                    }) 
+                })   
+            })
+        });
+   });
+};
+
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((resp) => {
+    let photo = resp.data;
+    printPhoto(photo);
+    openAndCloseOver();
 });
-
-
-
-
